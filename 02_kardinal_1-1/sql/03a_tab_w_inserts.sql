8206d57b2199d3f1b4a755711b307e35914e0b00
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-17 08:51:50 +0100

    updates 1:1

--- 02_kardinal_1-1/sql/03a_tab_w_inserts.sql
+++ 02_kardinal_1-1/sql/03a_tab_w_inserts.sql
@@ -1,4 +1,4 @@
--- 3. verbundene Tabellen + Inserts
+-- 3a. verbundene Tabellen + Inserts
 
 -- Vorbereitung
 DROP DATABASE IF EXISTS mydb;
@@ -24,7 +24,7 @@ INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Maus
 -- Inhalte: MT
 SELECT * FROM cats;
 
--- Detailtabelle: Verbindung zur MT über Fremdschlüssel
+-- Detailtabelle: Verbindung zur MT über Fremdschlüssel mit UNIQUE
 CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `srv_name` VARCHAR(45) NOT NULL,
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
   `cats_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_servants_cats_idx` (`cats_id` ASC) VISIBLE,
+  UNIQUE INDEX `cats_id_UNIQUE` (`cats_id` ASC) VISIBLE,
   CONSTRAINT `fk_servants_cats`
     FOREIGN KEY (`cats_id`)
     REFERENCES `mydb`.`cats` (`id`)
@@ -44,10 +45,6 @@ DESCRIBE servants;
 
 -- Inserts: DT (Detailtable)
 INSERT INTO `mydb`.`servants` (`id`, `srv_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Peter", 5, 2);
-
--- das sollte eigentlich NICHT gehen (Gleiche cats_id)
-#INSERT INTO `mydb`.`servants` (`id`, `srv_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Josef", 2, 2);
-
 INSERT INTO `mydb`.`servants` (`id`, `srv_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Josef", 2, 1);
 INSERT INTO `mydb`.`servants` (`id`, `srv_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Max", 12, 3);
 
