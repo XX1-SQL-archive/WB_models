f044d7c2939d131c42ea8c3fd797046a7b88222b
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-18 09:12:36 +0100

    Update 04_tab_selects.sql

--- 02_kardinal_1-1/sql/04_tab_selects.sql
+++ 02_kardinal_1-1/sql/04_tab_selects.sql
@@ -7,12 +7,39 @@ SELECT * FROM servants;
 -- Kreuzprodukt (Kartesisches Produkt)
 SELECT * FROM cats JOIN servants;
 
--- Inner Join 1
+-- Inner Join 1 / Gesamte Tabelle
 SELECT 
 	* 
 FROM cats INNER JOIN servants
 ON cats.id = servants.cats_id
 ;
 
+-- Inner Join 1 / Gesamte Tabelle
+SELECT 
+	*
+FROM cats INNER JOIN servants
+ON cats.id = servants.cats_id
+;
 
+-- Inner Join 2 (Wer dient wem?)
+-- Wer dient Grizabella?
+SELECT 
+	cat_name AS Katze,
+    srv_name AS Diener
+FROM cats INNER JOIN servants
+ON cats.id = servants.cats_id
+#WHERE cat_name = "Grizabella"
+#WHERE cat_name = "Mausi"
+WHERE srv_name = "Josef"
+;
 
+-- Inner Join 2a 
+-- Wem dient Josef? --> "X ist der Diener von Y"
+-- Josef + " ist der Diener von " + Grizabella
+SELECT 
+	concat(srv_name, " ist der Diener von ", cat_name,".") AS Dienstverhältnis
+FROM cats INNER JOIN servants
+ON cats.id = servants.cats_id
+#WHERE cat_name = "Grizabella"
+WHERE srv_name = "Josef"
+;
