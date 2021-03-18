dcddd386388e94c14a9dc652014717a15dd2c806
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-18 11:13:30 +0100

    update 1-n

--- 03_kardinal_1-n/sql/04_selects.sql
+++ 03_kardinal_1-n/sql/04_selects.sql
@@ -1,1 +1,29 @@
 -- 04. SELECTs
+
+-- Einzeltabellen
+SELECT * FROM cats;
+SELECT * FROM kittens;
+
+-- Inner Join 1 / Gesamte Kombi-Tabelle
+SELECT
+	*
+FROM cats INNER JOIN kittens
+ON cats.id = kittens.cats_id 
+;
+
+-- Inner Join 2 / Verwandschaft
+SELECT
+	cat_name AS Mutter,
+    kitten_name AS Kind,
+    concat(cat_name, " ist die Mutter von ", kitten_name) AS Verwandschaft
+FROM cats INNER JOIN kittens
+ON cats.id = kittens.cats_id 
+;
+
+-- Inner Join 3 / Wieviel Kinder haben die Katzen?
+SELECT
+	cat_name AS Katze,
+    count(cat_name) AS Kinder
+FROM cats INNER JOIN kittens ON cats.id = kittens.cats_id 
+GROUP BY cat_name # Wichtig bei Kombi Aggr./Nicht-Aggr.
+;
