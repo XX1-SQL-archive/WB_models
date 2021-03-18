f9e8091b0bc35c38822cfbed1cd118d2f3e8cd4d
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-18 12:02:17 +0100

    Update 03_selects.sql

--- 04a_kardinal_n-m/sql/03_selects.sql
+++ 04a_kardinal_n-m/sql/03_selects.sql
@@ -5,3 +5,13 @@ SELECT * FROM cats;
 SELECT * FROM servants;
 SELECT * FROM products;
 SELECT * FROM purchases;
+
+-- Inner Join 1 / Kombi (servants / products / purchases)
+SELECT
+	*
+FROM purchases 
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+;
+
+
