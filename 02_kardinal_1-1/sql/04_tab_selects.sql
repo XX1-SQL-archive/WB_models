8d605b0e901b5f3bc65b1a30d258b78dd450c34f
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-18 08:38:36 +0100

    updates

--- 02_kardinal_1-1/sql/04_tab_selects.sql
+++ 02_kardinal_1-1/sql/04_tab_selects.sql
@@ -1,3 +1,18 @@
 -- SELECTS TBA
 
+-- Einzeltabellen
+SELECT * FROM cats;
+SELECT * FROM servants;
+
+-- Kreuzprodukt (Kartesisches Produkt)
+SELECT * FROM cats JOIN servants;
+
+-- Inner Join 1
+SELECT 
+	* 
+FROM cats INNER JOIN servants
+ON cats.id = servants.cats_id
+;
+
+
 
