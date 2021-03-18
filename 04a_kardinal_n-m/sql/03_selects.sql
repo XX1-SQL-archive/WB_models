737851c7a73cf86431678b7fdb22da45b136a4f4
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-18 12:21:11 +0100

    Update 03_selects.sql

--- 04a_kardinal_n-m/sql/03_selects.sql
+++ 04a_kardinal_n-m/sql/03_selects.sql
@@ -16,12 +16,23 @@ INNER JOIN products ON products.id = purchases.products_id
 
 -- Welche Artikel hat Holger gekauft?
 SELECT
-	product_name AS Artikel
+    product_name AS Artikel
 FROM purchases 
 INNER JOIN servants ON servants.id = purchases.servants_id
 INNER JOIN products ON products.id = purchases.products_id
-#WHERE srv_name = "Holger"
-WHERE srv_name = "Peter"
+WHERE srv_name = "Holger"
+#WHERE srv_name = "Peter"
 ;
 
-
+-- Wieviel Produkte hat Holger gekauft?
+-- Wieviel Geld hat Holger ausgegeben?
+SELECT
+	srv_name AS Diener,
+    count(srv_name) AS Artikelanzahl  # Artikelanzahl
+FROM purchases 
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+GROUP BY srv_name
+HAVING srv_name = "Holger"
+#WHERE srv_name = "Peter"
+;
