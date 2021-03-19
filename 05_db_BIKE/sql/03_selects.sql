c3ae41ae76805dcac138e5392eafe4a8b99efe81
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-19 11:07:24 +0100

    Create 03_selects.sql

---
+++ 05_db_BIKE/sql/03_selects.sql
@@ -0,0 +1,64 @@
+USE bike;
+SHOW TABLES;
+
+/* Query 1
+Schreiben Sie einen Select-Befehl, 
+der aus der Relation (Tabelle) Personal die Namen aller Personen ermittelt, 
+die mehr als 3000 Euro verdienen.
+*/
+
+-- ges. Tabelle
+SELECT * FROM Personal;
+
+-- Query
+
+
+/* Query 2
+Geben Sie die Gesamtanzahl der für Aufträge reservierten Artikel aus 
+(die benötigten Informationen stehen in der Relation Reservierung).
+*/
+
+
+
+
+
+/* Query 3
+Geben Sie alle Artikel der Relation Lager aus, deren Bestand abzüglich 
+des Mindestbestands und der Reservierungen unter den Wert 3 gesunken ist. 
+Als Ausgabe werden Artikelnummer und Artikelbezeichnung erwartet.
+*/
+-- Bedingung:  (Bestand - Mindbest - Reserviert) < 3
+
+SELECT * FROM Lager; -- Tab. Lager
+SELECT * FROM Artikel; -- Tab. Artikel
+
+-- Vorbereitung: Tab. Lager + Bedingung
+
+
+
+
+-- Tab. Lager + Artikel m. INNER JOIN + Bedingung
+
+
+
+/* Query 4
+Aus wie vielen Einzelheiten bestehen alle zusammengesetzten Artikel?
+Falls ein Einzelteil wieder aus noch kleineren Einzelteilen besteht, 
+so ist dies nicht weiter zu berücksichtigen. Tab. Teilestruktur --> Anzahl
+*/
+
+SELECT * FROM Teilestruktur;
+
+
+
+/* Query 5
+Geben Sie alle Artikel aus, die vom Auftrag mit der Auftragsnummer 2 reserviert sind.
+Geben Sie dazu zu jedem Artikel die Artikelnummer, die Artikelbezeichnung und die Anzahl 
+der für diesen Auftrag reservierten Artikel aus. 
+*/
+
+SELECT * FROM Auftragsposten;
+SELECT * FROM Reservierung;
+
+
+
